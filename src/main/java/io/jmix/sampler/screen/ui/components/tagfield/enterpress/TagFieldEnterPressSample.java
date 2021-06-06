34b24a3c487c31eb8f38a421008c08aa2189388c
Roman Pinyazhin <pinyazhin@haulmont.com>
2021-06-06 17:29:38 +0400

    Add TagField samples Haulmont/jmix-sampler (39)

---
+++ src/main/java/io/jmix/sampler/screen/ui/components/tagfield/enterpress/TagFieldEnterPressSample.java
@@ -0,0 +1,46 @@
+package io.jmix.sampler.screen.ui.components.tagfield.enterpress;
+
+import io.jmix.core.Metadata;
+import io.jmix.sampler.entity.Product;
+import io.jmix.ui.Notifications;
+import io.jmix.ui.component.HasEnterPressHandler;
+import io.jmix.ui.component.TagField;
+import io.jmix.ui.screen.*;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@UiController("tagfield-enterpress")
+@UiDescriptor("tagfield-enterpress.xml")
+public class TagFieldEnterPressSample extends ScreenFragment {
+
+    @Autowired
+    private TagField<Product> tagFieldNewItem;
+
+    @Autowired
+    private Metadata metadata;
+    @Autowired
+    private Notifications notifications;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        tagFieldNewItem.setEnterPressHandler(new TagField.NewTagProvider<Product>() {
+
+            @Nullable
+            @Override
+            public Product create(String text) {
+                Product product = metadata.create(Product.class);
+                product.setName(text);
+                return product;
+            }
+        });
+    }
+
+    @Install(to = "tagFieldEnterHandler", subject = "enterPressHandler")
+    private void tagFieldEnterHandlerEnterPressHandler(HasEnterPressHandler.EnterPressEvent enterPressEvent) {
+        notifications.create(Notifications.NotificationType.TRAY)
+                .withCaption("Entered text: " + enterPressEvent.getText())
+                .show();
+    }
+
+
+}
---
+++ src/main/java/io/jmix/sampler/screen/ui/components/tagfield/tagclick/TagFieldTagClickSample.java
@@ -0,0 +1,27 @@
+package io.jmix.sampler.screen.ui.components.tagfield.tagclick;
+
+import io.jmix.sampler.entity.Product;
+import io.jmix.ui.Notifications;
+import io.jmix.ui.component.TagField;
+import io.jmix.ui.screen.ScreenFragment;
+import io.jmix.ui.screen.Subscribe;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@UiController("tagfield-tagclick")
+@UiDescriptor("tagfield-tagclick.xml")
+public class TagFieldTagClickSample extends ScreenFragment {
+
+    @Autowired
+    private Notifications notifications;
+
+    @Subscribe("tagField")
+    public void onTagFieldTagClick(TagField.TagClickEvent<Product> event) {
+        notifications.create(Notifications.NotificationType.TRAY)
+                .withCaption(event.getItem().getName())
+                .show();
+    }
+
+
+}
